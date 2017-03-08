9f904a471c8734c1b8fe77256d91f1792de07988
Guillem Borrell <guillemborrell@gmail.com>
2017-03-08 11:40:55 +0100

    Now ports is passed as argument.

--- pyledger/config.py
+++ pyledger/config.py
@@ -4,8 +4,8 @@ from collections import namedtuple
 
 
 if len(sys.argv) > 2 and 'test' in sys.argv[2]:
-    argstype = namedtuple('Arguments', ['db', 'debug', 'sync'])
-    args = argstype(db='sqlite://', debug=False, sync=True)
+    argstype = namedtuple('Arguments', ['db', 'debug', 'sync', 'port'])
+    args = argstype(db='sqlite://', debug=False, sync=True, port=8888)
 
 else:
     parser = argparse.ArgumentParser(description='Run the PyLedger server')
@@ -13,4 +13,5 @@ else:
                         type=str, default="sqlite://")
     parser.add_argument('--sync', action='store_true')
     parser.add_argument('--debug', action='store_true', default=False)
+    parser.add_argument('--port', type=int, default=8888)
     args = parser.parse_args()
--- pyledger/server.py
+++ pyledger/server.py
@@ -1,9 +1,10 @@
 from pyledger.handlers import make_application
+from pyledger.config import args
 import tornado.ioloop
 
 
 def main():
     application = make_application()
-    application.listen(8888)
+    application.listen(args.port)
     tornado.ioloop.IOLoop.instance().start()
 
