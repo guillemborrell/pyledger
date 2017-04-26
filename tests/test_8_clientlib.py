e045c7816168d573be6b8fd5c798942c980b26d4
Guillem Borrell <guillemborrell@gmail.com>
2017-04-26 09:52:06 +0200

    Moved client stuff to the client package

--- tests/test_8_clientlib.py
+++ tests/test_8_clientlib.py
@@ -14,7 +14,7 @@
 #    You should have received a copy of the GNU Affero General Public License
 #    along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-from pyledger2.clientlib import *
+from pyledger2.client.lib import *
 from pyledger2.handlers import handle_request
 
 
